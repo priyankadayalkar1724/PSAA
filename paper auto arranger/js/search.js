bf31e05fd4ec7f46a968346713826e9d6b0cd2d1
priyankadayalkar1724 <141647623+priyankadayalkar1724@users.noreply.github.com>
2023-08-11 13:53:57 +0530

    Add files via upload

---
+++ paper auto arranger/js/search.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.navbar');
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
